a3ecd2190e149a656df5a989728d8ea8b526cdaf
Chirag8405 <chiragpoornamath@gmail.com>
2026-09-22 23:03:57 +0530

    fix(artisan): remove syncingAadhaar from dependency array in useEffect

--- frontend/app/artisan/page.js
+++ frontend/app/artisan/page.js
@@ -323,7 +323,7 @@ export default function ArtisanPage() {
     return () => {
       active = false;
     };
-  }, [wallet, isAnonVerified, isArtisanRegistered, aadhaarSyncedOnChain, autoSyncAttempted, syncingAadhaar]);
+  }, [wallet, isAnonVerified, isArtisanRegistered, aadhaarSyncedOnChain, autoSyncAttempted]);
 
   async function onConnect() {
     try {
